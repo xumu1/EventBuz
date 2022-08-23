170a66ea423a10d9353c7f097546115e7e24bc01
sunhaolong.xumu <sunhaolong.xumu@bytedance.com>
2022-08-23 11:20:11 +0800

    增加rpc网络功能

--- event_buz_test.go
+++ event_buz_test.go
@@ -4,9 +4,7 @@
 package EventBuz
 
 import (
-	"fmt"
 	"testing"
-	"time"
 )
 
 var topic string
@@ -18,27 +16,27 @@ func init() {
 }
 
 func Test_AsyncHandler(t *testing.T) {
-	myHandler := &EventHandlerImpl{
-		once:  false,
-		async: false,
-		eventHandlerFuc: func(formData string) error {
-			fmt.Println(formData)
-			time.Sleep(3 * time.Second)
-			return nil
-		},
-	}
-	buz.Subscribe(topic, myHandler)
-	buz.Publish(topic, map[string]interface{}{
-		"a": "aa",
-	})
-	fmt.Printf("now time is: %v\n", time.Now())
-	buz.Publish(topic, map[string]interface{}{
-		"b": "bb",
-	})
-	fmt.Printf("now time is: %v\n", time.Now())
-	buz.WaitAsync()
-	fmt.Printf("now time is: %v\n", time.Now())
-	fmt.Println("it is fine.")
+	//myHandler := &EventHandlerImpl{
+	//	once:  false,
+	//	async: false,
+	//	eventHandlerFuc: func(formData string) error {
+	//		fmt.Println(formData)
+	//		time.Sleep(3 * time.Second)
+	//		return nil
+	//	},
+	//}
+	//buz.Subscribe(topic, myHandler)
+	//buz.Publish(topic, map[string]interface{}{
+	//	"a": "aa",
+	//})
+	//fmt.Printf("now time is: %v\n", time.Now())
+	//buz.Publish(topic, map[string]interface{}{
+	//	"b": "bb",
+	//})
+	//fmt.Printf("now time is: %v\n", time.Now())
+	//buz.WaitAsync()
+	//fmt.Printf("now time is: %v\n", time.Now())
+	//fmt.Println("it is fine.")
 
 }
 
@@ -64,32 +62,32 @@ func Test_base_function_1(t *testing.T) {
 	//})
 }
 func Test_base_function_2(t *testing.T) {
-	handler := &EventHandlerImpl{
-		eventHandlerFuc: func(formData string) error {
-			fmt.Println(formData)
-			return nil
-		},
-	}
-	buz.Subscribe(topic, handler)
-	buz.Publish(topic, map[string]interface{}{
-		"a": "aa",
-	})
-	buz.UnSubscribe(topic, handler)
-	buz.Publish(topic, map[string]interface{}{
-		"b": "bb",
-	})
-	onceHandler := &EventHandlerImpl{
-		once: true,
-		eventHandlerFuc: func(formData string) error {
-			fmt.Println(formData)
-			return nil
-		},
-	}
-	buz.Subscribe(topic, onceHandler)
-	buz.Publish(topic, map[string]interface{}{
-		"c": "cc",
-	})
-	buz.Publish(topic, map[string]interface{}{
-		"d": "dd",
-	})
+	//handler := &EventHandlerImpl{
+	//	eventHandlerFuc: func(formData string) error {
+	//		fmt.Println(formData)
+	//		return nil
+	//	},
+	//}
+	//buz.Subscribe(topic, handler)
+	//buz.Publish(topic, map[string]interface{}{
+	//	"a": "aa",
+	//})
+	//buz.UnSubscribe(topic, handler)
+	//buz.Publish(topic, map[string]interface{}{
+	//	"b": "bb",
+	//})
+	//onceHandler := &EventHandlerImpl{
+	//	once: true,
+	//	eventHandlerFuc: func(formData string) error {
+	//		fmt.Println(formData)
+	//		return nil
+	//	},
+	//}
+	//buz.Subscribe(topic, onceHandler)
+	//buz.Publish(topic, map[string]interface{}{
+	//	"c": "cc",
+	//})
+	//buz.Publish(topic, map[string]interface{}{
+	//	"d": "dd",
+	//})
 }
--- network_event_buz_test.go
+++ network_event_buz_test.go
@@ -3,10 +3,28 @@
 
 package EventBuz
 
+import "testing"
+
 var netTopic string
 var netBuz Bus
 
 func init() {
 	topic = "test-topic"
 	buz = NewEventBuz()
+}
+
+func Test_Client(t *testing.T) {
+	client := NewClient(":7777", "/_client", NewEventBuz())
+	client.Start()
+	println(client.clientService.started)
+	client.Stop()
+	println(client.clientService.started)
+}
+
+func Test_Server(t *testing.T) {
+	client := NewClient(":7777", "/_client", NewEventBuz())
+	server := NewServer(":8888", "/_server", NewEventBuz())
+	client.Start()
+	server.Start()
+
 }
